5d2243eb5653d48d43c387f6c17e45b06cdc734c
leeyunhome <leeyunhome@daum.net>
2020-02-25 18:45:52 +0900

    up to page275

---
+++ Chapter9/BasicProblem0902/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem0902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            int totalScore = 0;
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.Write(i);
+                Console.WriteLine("번째 과목의 성적을 입력하세요.");
+                scores[i] = int.Parse(Console.ReadLine());
+                totalScore += scores[i];
+            }
+
+            Console.Write("총점은 ");
+            Console.Write(totalScore);
+            Console.WriteLine("점입니다.");
+        }
+    }
+}
---
+++ Chapter9/BasicProblem0903/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem0903
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 2; j <= 8; j += 2)
+            {
+                for (int i = 1; i <=9; ++i)
+                {
+                    Console.Write(j);
+                    Console.Write("x");
+                    Console.Write(i);
+                    Console.Write(" = ");
+                    Console.Write(j * i);
+                    Console.Write(" ");
+                    //if (j%2 == 0)
+                    //{
+                    //    Console.Write(j);
+                    //    Console.Write("x");
+                    //    Console.Write(i);
+                    //    Console.Write(" = ");
+                    //    Console.WriteLine(j * i);
+                    //}
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
