09724769bc62b42ce6de239ed77290a25a9b8a09
leeyunhome <leeyunhome@daum.net>
2020-02-25 21:45:17 +0900

    save

---
+++ Chapter10/Deep10_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deep10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int target = 0;
+
+            target = random.Next(1, 101);
+
+            Console.Write("컴퓨터가 고른 숫자 : ");
+            Console.WriteLine(target);
+            while (true)
+            {
+                
+                Console.WriteLine("숫자를 입력하세요.");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > target)
+                {
+                    Console.WriteLine("더 작은 숫자입니다");
+                }
+                else if (num < target)
+                {
+                    Console.WriteLine("더 큰 숫자입니다.");
+                }
+                else// if (num == target)
+                {
+                    Console.WriteLine("정답입니다.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Chapter10/Deep10_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deep10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int target = random.Next(1, 4);
+            int[] num = new int[3];
+
+            Console.Write("컴퓨터가 낸 것: ");
+            Console.WriteLine(target);
+            
+            Console.WriteLine("1: 가위, 2: 바위, 3: 보");
+            while (true)
+            {
+                for (int i = 0; i < 3; ++i)
+                {
+                    Console.WriteLine("가위바위보! : ");
+
+                    num[i] = int.Parse(Console.ReadLine());
+                    if (target == i + 1)
+                    {
+                        if (num[i] == target)
+                        {
+                            Console.WriteLine("비겼습니다.");
+                        }
+                        else if (num[i] == 2)
+                        {
+                            Console.WriteLine("이겼습니다.");
+                            break;
+                        }
+                        else if (num[i] == 3)
+                        {
+                            Console.WriteLine("졌습니다.");
+                            break;
+                        }
+                     
+                    }
+                }
+                
+
+
+            }
+        }
+    }
+}
