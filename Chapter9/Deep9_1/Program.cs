11ffa4086177b5f594b1dc0b2ee3bdde3d3af457
leeyunhome <leeyunhome@daum.net>
2020-02-25 20:30:13 +0900

    up to page292

---
+++ Chapter9/Deep9_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Deep9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] passcodeNumbers = { 6, 2, 1, 9, 4, 7 };
+
+            int passcodeLength = 6;
+            int[] userInput = new int[passcodeLength];
+
+            int count;
+
+            Console.WriteLine("비밀번호가 5회 일치하지 않으면 5분 기다려야합니다.");
+            for (count = 0; count < 5; ++count)
+            {
+                for (int passcodeIndex = 0; passcodeIndex < passcodeLength; ++passcodeIndex)
+                {
+                    Console.Write(passcodeIndex + 1);
+                    Console.WriteLine("번째 숫자를 넣어주세요.");
+                    userInput[passcodeIndex] = int.Parse(Console.ReadLine());
+                }
+
+                bool isPasswordCorrect = true;
+                for (int passcodeIndex = 0; passcodeIndex < passcodeLength; ++passcodeIndex)
+                {
+                    if (userInput[passcodeIndex] != passcodeNumbers[passcodeIndex])
+                    {
+                        isPasswordCorrect = false;
+                        Console.Write(count + 1);
+                        Console.WriteLine("회 비밀번호가 틀렸습니다.");
+                        break;
+                    }
+                }
+
+                if (isPasswordCorrect == true)
+                {
+                    Console.WriteLine("문이 열렸습니다.");
+                    break;
+                }
+
+                if (count == 4)
+                {
+                    Console.Write(count + 1);
+                    Console.WriteLine("번의 입력횟수를 초과했습니다. 5분후 다시 시도해주세요.");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter9/Deep9_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Deep9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] scores = new int[5,10];
+
+            int j;
+            for (j = 0; j < 5; ++j)
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Console.Write(j + 1);
+                    Console.Write("반 ");
+                    Console.Write(i + 1);
+                    Console.WriteLine("번 학생의 성적을 입력하세요.");
+                    scores[j,i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (j = 0; j < 5; ++j)
+            {
+                Console.Write(j + 1);
+                Console.Write("반 평균은");
+                int totalScore = 0;
+                for (int i = 0; i < 10; ++i)
+                {
+                    totalScore += scores[j, i];
+                }
+                Console.Write(totalScore / 10);
+                Console.WriteLine("점 입니다.");
+            }
+
+        }
+    }
+}
