d59df04bb45798bb1f449489cf3e160ba09efc31
leeyunhome <leeyunhome@daum.net>
2020-02-25 22:21:43 +0900

    page308 myself

--- Chapter10/Deep10_2/Program.cs
+++ Chapter10/Deep10_2/Program.cs
@@ -10,43 +10,78 @@ namespace Deep10_2
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int target = random.Next(1, 4);
-            int[] num = new int[3];
-
-            Console.Write("컴퓨터가 낸 것: ");
-            Console.WriteLine(target);
+            Console.WriteLine("가위 바위 보 게임을 해보자");
             
-            Console.WriteLine("1: 가위, 2: 바위, 3: 보");
             while (true)
             {
-                for (int i = 0; i < 3; ++i)
-                {
-                    Console.WriteLine("가위바위보! : ");
+                Random random = new Random();
+                int target = random.Next(1, 4);
 
-                    num[i] = int.Parse(Console.ReadLine());
-                    if (target == i + 1)
+                //Console.Write("컴퓨터가 낸 것: ");
+                Console.WriteLine(target);
+
+                Console.WriteLine("컴퓨터는 이미 정했습니다. 가위바위보를 숫자로 입력해주세요.");
+                Console.WriteLine("1: 가위, 2: 바위, 3: 보");
+
+                int num = int.Parse(Console.ReadLine());
+
+                if (target == 1)
+                {
+                    if (num == 2)
                     {
-                        if (num[i] == target)
-                        {
-                            Console.WriteLine("비겼습니다.");
-                        }
-                        else if (num[i] == 2)
-                        {
-                            Console.WriteLine("이겼습니다.");
-                            break;
-                        }
-                        else if (num[i] == 3)
-                        {
-                            Console.WriteLine("졌습니다.");
-                            break;
-                        }
-                     
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else if (num == 3)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        Console.Write("컴퓨터가 낸 것: ");
+                        Console.WriteLine(target);
+                        break;
+                    }
+                    else //if (num == 1)
+                    {
+                        Console.WriteLine("비겼습니다.");
                     }
                 }
-                
-
-
+                else if (target == 2)
+                {
+                    if (num == 1)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        Console.Write("컴퓨터가 낸 것: ");
+                        Console.WriteLine(target);
+                        break;
+                    }
+                    else if (num == 3)
+                    {
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else //if (num == 2)
+                    {
+                        Console.WriteLine("비겼습니다.");
+                    }
+                }
+                else if (target == 3)
+                {
+                    if (num == 1)
+                    {
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else if (num == 2)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        Console.Write("컴퓨터가 낸 것: ");
+                        Console.WriteLine(target);
+                        break;
+                    }
+                    else //if (num == 3)
+                    {
+                        Console.WriteLine("비겼습니다.");
+                    }
+                }
             }
         }
     }
