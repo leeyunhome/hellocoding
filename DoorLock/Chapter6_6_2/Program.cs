0cf6fd66dcbf8124703d805290fe4b759b8274a1
leeyunhome <leeyunhome@daum.net>
2020-02-25 13:06:00 +0900

    upto page184

---
+++ DoorLock/Chapter6_6_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter6_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > 20 && userInput % 3 == 0)
+            {
+                Console.WriteLine("20보다 큰 3의 배수입니다.");
+            }
+        }
+    }
+}
